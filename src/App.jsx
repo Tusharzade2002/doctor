fa3b981e7ebd8f15ebbc29933a4bd236e439f8eb
Tusharzade <tusharzade29@gmail.com>
2025-04-30 15:12:44 +0530

    create new pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Mainpage from './Views/Mainpage';
 import NotFound from './Views/NotFound';
 import Dashboard from './Views/Dashboard';
 import Patient from './Views/Patient';
+import Appointment from './Views/Appointment';
+import PatientDetail from './Views/PatientDetail';
+import Department from './Views/Department';
 function App() {
 
   return (
@@ -21,10 +24,14 @@ function App() {
       <Route path="/" element={<Registration />} />
       <Route path="/dashboard" element={<Dashboard/>} />
       <Route path="/patient" element={<Patient/>} />
+      <Route path="/appointment" element={<Appointment/>} />
       <Route path="/home" element={<Home />} />
-      <Route path="/doctor/login" element={<DoctorLogin />} />
-      <Route path="/doctor/registration" element={<DoctorRegistration />} />
       <Route path="/*" element={<NotFound />} />
+      <Route path="/patientdetail" element={<PatientDetail />} /> 
+       <Route path="/department" element={<Department />} />
+      {/* <Route path="/doctor/login" element={<DoctorLogin />} />
+      <Route path="/doctor/registration" element={<DoctorRegistration />} /> */}
+    
     </Routes>
   </BrowserRouter>
  
